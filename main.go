edaf11b7b12f0f1e1e04e61c219e134e0cdf4983
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-02-24 08:02:32 +0100

    recipe model

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type Recipe struct {
+	Name         string   `json:"Name"`
+	Keywords     []string `json:"Keywords"`
+	Ingredients  []string `json:"Ingredients"`
+	Instructions string   `json:"Instructions"`
+	PublishedAt  string   `json:"PublishedAt"`
+}
+
 func main() {
 	router := gin.Default()
 	router.Run()
