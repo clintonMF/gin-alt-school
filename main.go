296674d63decc2853b7faae92178af8bd77f804d
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-02-24 07:56:56 +0100

    boilerplate

--- main.go
+++ main.go
@@ -2,15 +2,7 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func IndexHandler(c *gin.Context) {
-	username := c.Params.ByName("username")
-	c.JSON(200, gin.H{
-		"message": "Hello " + username,
-	})
-}
-
 func main() {
 	router := gin.Default()
-	router.GET("/:username", IndexHandler)
 	router.Run()
 }
